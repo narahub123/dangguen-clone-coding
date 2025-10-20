3c749b795dfa914a0e7887032eaeb814d20ea1be
narahub123 <parknara0213@gmail.com>
2025-10-21 00:29:34 +0900

    Feat: SellItemLocationPage 생성

--- client/src/pages/SellItemLocationPage.tsx
+++ client/src/pages/SellItemLocationPage.tsx
@@ -1,3 +1,24 @@
+import { PageHeader } from "../ui";
+
 export const SellItemLocationPage = () => {
-  return <div></div>;
+  return (
+    <div className="w-[500px] h-screen flex flex-col">
+      <PageHeader text="" />
+      <main className="flex-1">
+        <section className="p-4">
+          <p className="font-bold">이웃과 만나서</p>
+          <p className="font-bold">거래하고 싶은 장소를 선택해주세요.</p>
+          <p className="text-sm">
+            만나서 거래할 때는 누구나 찾기 쉬운 공공장소가 좋아요
+          </p>
+        </section>
+        <section>맵</section>
+      </main>
+      <footer className="p-4 sticky bottom-0 bg-white">
+        <button className="w-full bg-orange-400 p-2 rounded-lg text-white">
+          선택 완료
+        </button>
+      </footer>
+    </div>
+  );
 };
