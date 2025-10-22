ad560861c05a64e492a4ecca9ba56ffe80a42d25
narahub123 <parknara0213@gmail.com>
2025-10-22 15:29:58 +0900

    Feat: UserInfoSection 생성

--- client/src/asset/images/index.ts
+++ client/src/asset/images/index.ts
@@ -1,5 +1,5 @@
 import example1 from "./example1.webp";
 import example2 from "./example2.webp";
 import example3 from "./example3.webp";
-
-export { example1, example2, example3 };
+import profileImage from "./default-profile.png";
+export { example1, example2, example3, profileImage };
--- client/src/ui/post/sections/UserInfoSection.tsx
+++ client/src/ui/post/sections/UserInfoSection.tsx
@@ -1,3 +1,29 @@
+import { Link } from "react-router-dom";
+import { profileImage } from "../../../asset";
+
 export const UserInfoSection = () => {
-  return <section>사용자 정보 섹션</section>;
+  return (
+    <section className="p-4">
+      <div className="w-full flex justify-between">
+        <div className="flex items-center gap-2">
+          {/* 사용자 사진 */}
+          <figure className="w-16 h-16 rounded-full border">
+            <img className="w-16 h-16 rounded-full " src={profileImage} />
+          </figure>
+          <div>
+            <p>사용자 이름</p>
+            <p>지역 이름</p>
+          </div>
+        </div>
+        <div>
+          <div>32.7😪</div>
+          <div>
+            <Link to="/" className="text-sm">
+              매너온도
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
