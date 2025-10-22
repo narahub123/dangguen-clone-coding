79c5ee37a98ad43972fee4b6b78aec64deac324b
narahub123 <parknara0213@gmail.com>
2025-10-22 15:59:48 +0900

    Feat: UserProductsSection, SimiliarProductsSection 생성

--- client/src/ui/post/sections/SimiliarProductsSection.tsx
+++ client/src/ui/post/sections/SimiliarProductsSection.tsx
@@ -1,3 +1,10 @@
+import { LinkButton } from "../../sell-item";
+
 export const SimiliarProductsSection = () => {
-  return <section>지금 물품과 비슷해요</section>;
+  return (
+    <section>
+      <LinkButton text="지금 보고 있는 물품과 비슷해요" to="" />
+      <div>유사한 물품 목록</div>
+    </section>
+  );
 };
--- client/src/ui/post/sections/UserProductsSection.tsx
+++ client/src/ui/post/sections/UserProductsSection.tsx
@@ -1,3 +1,10 @@
+import { LinkButton } from "../../sell-item";
+
 export const UserProductsSection = () => {
-  return <section>사용자 물품</section>;
+  return (
+    <section>
+      <LinkButton text="지금 보고 있는 물품과 비슷해요" to="" />
+      <div>사용자의 판매 물품 목록</div>
+    </section>
+  );
 };
