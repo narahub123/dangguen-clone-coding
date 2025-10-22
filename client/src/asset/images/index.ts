a546a20e1d9bd358a4e60ffe8ac284487113922f
narahub123 <parknara0213@gmail.com>
2025-10-22 15:20:38 +0900

    Feat: ImageSection 생성

---
+++ client/src/asset/images/index.ts
@@ -0,0 +1,5 @@
+import example1 from "./example1.webp";
+import example2 from "./example2.webp";
+import example3 from "./example3.webp";
+
+export { example1, example2, example3 };
--- client/src/ui/post/sections/ImageSection.tsx
+++ client/src/ui/post/sections/ImageSection.tsx
@@ -1,3 +1,13 @@
+import { example1, example2, example3 } from "../../../asset";
+
 export const ImageSection = () => {
-  return <section>이미지 섹션</section>;
+  const images = [example1, example2, example3];
+  return (
+    <section>
+      <div className="w-full aspect-square relative">
+        <img src={example1} alt="" className="w-full aspect-square" />
+        <span className="absolute bottom-2 right-2 px-2 bg-black/50 rounded-xl text-white">{`1/${images.length}`}</span>
+      </div>
+    </section>
+  );
 };
