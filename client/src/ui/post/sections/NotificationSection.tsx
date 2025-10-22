edbe02351697d2d501cabab379a66884a03ed88e
narahub123 <parknara0213@gmail.com>
2025-10-22 16:06:03 +0900

    Feat: NotificationSection 생성

--- client/src/ui/post/sections/NotificationSection.tsx
+++ client/src/ui/post/sections/NotificationSection.tsx
@@ -1,3 +1,15 @@
+import { LuBell } from "react-icons/lu";
+
 export const NotificationSection = () => {
-  return <section>알림 설정</section>;
+  return (
+    <section className="p-4">
+      <div className="w-full flex justify-between">
+        <p className="max-w-[300px]">{`이웃들이 ${"맥북에러 m1 실버"} 게시글을 올리면 바로 알려들일까요?`}</p>
+        <button className="shrink-0 text-sm px-3 py-2 bg-gray-200 rounded-3xl flex items-center justify-center gap-1">
+          <LuBell />
+          알림 받기
+        </button>
+      </div>
+    </section>
+  );
 };
